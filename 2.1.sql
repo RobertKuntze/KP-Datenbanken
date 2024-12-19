9c64a96dc6f0cfe0ea596831762b070e92558bdd
Robert Kuntze <robertkuntze23@googlemail.com>
2024-12-19 13:00:27 +0100

    two complex queries: datetime, window, cte, aggregation

---
+++ 2.1.sql
@@ -0,0 +1,24 @@
+-- rank users over their total votes per posttype
+EXPLAIN ANALYZE
+WITH VoteStats AS (
+    SELECT 
+        u.Id AS UserId,
+        u.DisplayName,
+        p.PostTypeId,
+        SUM(CASE WHEN v.VoteTypeId = 2 THEN 1 ELSE 0 END) AS UpvotesReceived,
+        SUM(CASE WHEN v.VoteTypeId = 3 THEN 1 ELSE 0 END) AS DownvotesReceived
+    FROM users u
+    JOIN posts p ON u.Id = p.OwnerUserId
+    LEFT JOIN votes v ON p.Id = v.PostId
+    GROUP BY u.Id, u.DisplayName, p.PostTypeId
+)
+SELECT 
+    UserId,
+    DisplayName,
+    PostTypeId,
+    UpvotesReceived,
+    DownvotesReceived,
+    UpvotesReceived - DownvotesReceived AS NetVotes,
+    RANK() OVER (PARTITION BY PostTypeId ORDER BY UpvotesReceived - downvotesReceived DESC) AS RankWithinPostType
+FROM VoteStats
+ORDER BY netvotes DESC;
---
+++ 3.1.sql
@@ -0,0 +1,18 @@
+-- compare percentage of answered questions by year
+EXPLAIN ANALYZE
+WITH QuestionsAnswered AS (
+    SELECT 
+        extract(YEAR FROM p.CreationDate) AS Year,
+        COUNT(p.Id) AS TotalQuestions,
+        COUNT(CASE WHEN p.AcceptedAnswerId IS NOT NULL THEN 1 END) AS AnsweredQuestions
+    FROM posts p
+    WHERE p.PostTypeId = 1 -- Questions only
+    GROUP BY extract(YEAR FROM p.CreationDate)
+)
+SELECT 
+    Year,
+    TotalQuestions,
+    AnsweredQuestions,
+    (AnsweredQuestions * 100.0 / TotalQuestions) AS AnsweredPercentage
+FROM QuestionsAnswered
+ORDER BY Year;
